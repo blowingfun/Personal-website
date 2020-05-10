4a95c4a7e2eb8afe11a68cfb5cef91a4c2b5e13c
PaulChang <54823303+blowingfun@users.noreply.github.com>
2020-05-10 00:46:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+//to slow down the video playspeed
+var vid = document.getElementById("background");
+vid.playbackRate = 0.4;
+
+//slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+  if (n == 1) {document.getElementById("under_bar").style.marginLeft = "33%";}
+  else if (n == 2){document.getElementById("under_bar").style.marginLeft = "47%";}
+  else if (n == 3){document.getElementById("under_bar").style.marginLeft = "61%";}
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+ 
+  slides[slideIndex-1].style.display = "block";  
+}
