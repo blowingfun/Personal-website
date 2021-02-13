16408216f510cab032725d3482cd80d9ab886deb
Paul Chang <changpaul333@gmail.com>
2021-02-13 16:49:49 -0500

    adding flicker project to webiste

--- script.js
+++ script.js
@@ -7,24 +7,32 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
-  if (n == 1) {document.getElementById("under_bar").style.marginLeft = "33%";}
-  else if (n == 2){document.getElementById("under_bar").style.marginLeft = "47%";}
-  else if (n == 3){document.getElementById("under_bar").style.marginLeft = "61%";}
+  showSlides((slideIndex = n));
+  if (n == 1) {
+    document.getElementById("under_bar").style.marginLeft = "33%";
+  } else if (n == 2) {
+    document.getElementById("under_bar").style.marginLeft = "47%";
+  } else if (n == 3) {
+    document.getElementById("under_bar").style.marginLeft = "61%";
+  }
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+  if (n > slides.length) {
+    slideIndex = 1;
   }
- 
-  slides[slideIndex-1].style.display = "block";  
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
 }
